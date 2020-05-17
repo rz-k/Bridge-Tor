efd86a9b8ef8b4e3d35fd998c8e88d3603d600c8
رزا <36600639+reza-tanha@users.noreply.github.com>
2020-05-17 13:59:50 +0430

    Update B-tor.py

--- B-tor.py
+++ B-tor.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import time
+import platform
+import os
 from colorama import Fore
 from colorama import init
 
@@ -12,8 +14,8 @@ class Config_Tor(object):
         self.torrc = "/etc/tor/torrc"
         self.report_t = r'''
 Already configured . If the Tor service doesn't work for you , 
-Please Configure Manually Or Report Support Via Github , Telegram , Youtube, !!!
-Github : https://github.com/reza-tanha/
+Please Configure Manually Or Report Support Via Ip_info , Telegram , Youtube, !!!
+Ip_info : https://github.com/reza-tanha/
 Telegram : https://T.me/S3CURITY_GARY
 Youtube : https://bit.ly/2yas3rm'''
         self.text_1 = r''' 
@@ -31,7 +33,7 @@ Bridge obfs4 95.216.186.68:1192 CD6A3B2F22509345DB47F64B2A26FB6A601A1FA0 cert=1g
 
                     +++++++++++++++++++++++++++++++++++++++++++++++++++++
                     #    Tor Bridge Script                              #
-                    #    Version 1.0                                    #
+                    #    Version 1.2.0                                  #
                     #    Github : https://github.com/reza-tanha/        #
                     #    Telegram : T.me/S3CURITY_GARY                  #
                     #    Youtube : https://bit.ly/2yas3rm               #
@@ -48,12 +50,71 @@ Bridge obfs4 95.216.186.68:1192 CD6A3B2F22509345DB47F64B2A26FB6A601A1FA0 cert=1g
             time.sleep(0.002)
         print()
 
-    def install_de(self):
+    #=====================================
+
+    def is_obfsproxy(self):
+        x = os.popen("dpkg -s obfsproxy |grep -Poi 'status: \K\S\w.*'").read()
+        if x.strip() == 'install ok installed':
+            return True
+        else:
+            return False
+
+    def is_obfs4proxy(self):
+        x = os.popen("dpkg -s obfs4proxy |grep -Poi 'status: \K\S\w.*'").read()
+        if x.strip() == 'install ok installed':
+            return True
+        else:
+            return False
+
+    def getMachine(self):
+        x = platform.machine()
+        if x.strip() == 'x86_64':
+            return '64'
+            # print('64')
+        else:
+            return '32'
+            # print("32")
+
+    def ins_obfsproxy(self):
         try:
-            call(["sudo", "apt-get", "install", "obfs4proxy", "obfsproxy", "-y"], stdout=PIPE)
+            call(["sudo", "dpkg", "-i", "obfs/obfsproxy_all.deb"], stdout=PIPE)
         except:
-            print(Fore.RED + "Please manually install obfs4proxy, obfsproxy, tor")
-            print(Fore.CYAN + "sudo apt-get install obfs4proxy obfsproxy tor")
+            pass
+
+    def ins_obf4sproxy_64(self):
+        try:
+            call(["sudo", "dpkg", "-i", "obfs/obfs4proxy_amd64.deb"], stdout=PIPE)
+        except:
+            pass
+
+    def ins_obf4sproxy_32(self):
+        try:
+            call(["sudo", "dpkg", "-i", "obfs/obfsproxy_all.deb"], stdout=PIPE)
+        except:
+            pass
+
+    #=====================================
+
+    def install_obfs4proxy(self):
+        try:
+            call(["sudo", "apt-get", "install", "obfs4proxy", "-y"], stdout=PIPE)
+            if self.getMachine() == '64':
+                self.ins_obf4sproxy_64()
+                print(Fore.LIGHTGREEN_EX + "[✅] Install obfs4proxy [✅]")
+            else:
+                self.ins_obf4sproxy_32()
+                print(Fore.LIGHTGREEN_EX + "[✅] Install obfs4proxy [✅]")
+        except:
+            pass
+    def install_obfsproxy(self):
+        try:
+            call(["sudo", "apt-get", "install", "obfsproxy", "-y"], stdout=PIPE)
+            if self.is_obfsproxy() != True:
+                self.ins_obfsproxy()
+                print(Fore.LIGHTGREEN_EX + "[✅] Install obfsproxy [✅]")
+
+        except:
+            self.ins_obfsproxy()
 
 
     def check_to_text(self):
@@ -87,15 +148,21 @@ Bridge obfs4 95.216.186.68:1192 CD6A3B2F22509345DB47F64B2A26FB6A601A1FA0 cert=1g
         print()
 
 if __name__ == '__main__':
+
     T = Config_Tor()
     T.banner()
-    T.install_de()
+#    T.getMachine()
+    if T.is_obfs4proxy() != True:
+        T.install_obfs4proxy()
+    if T.is_obfsproxy() != True:
+        T.install_obfsproxy()
+
     if T.check_to_text():
         T.text_exiest()
     else:
         T.check_text_one()
         T.check_text_tow()
-        call(["service", "tor", "restart"])
-        print(Fore.LIGHTGREEN_EX + "Successfully configured :) ")
+        call(["service", "GRATOR", "restart"])
+        print(Fore.LIGHTGREEN_EX + "[✅] Successfully configured [✅] ")
 
 
