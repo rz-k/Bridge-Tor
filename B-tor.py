be110d654878094e4ae05a3640075fa5c9ec1f3c
رزا <36600639+reza-tanha@users.noreply.github.com>
2020-07-17 14:50:15 +0430

    Update B-tor.py

--- B-tor.py
+++ B-tor.py
@@ -24,9 +24,8 @@ ClientTransportPlugin obfs3 exec /usr/bin/obfsproxy managed
 ClientTransportPlugin obfs4 exec /usr/bin/obfs4proxy managed
 '''
         self.text_2 = r'''
-Bridge obfs4 161.97.248.185:9030 E795F5705ADF1EC8A9A525E2AA82CABF025457A0 cert=Y5WYkpNJ+tHaeMawMfmYUWAZekysqHXX856uW5q6BWrJQVEydVzVFkID/2bUgVeGLYmvLg iat-mode=0
-Bridge obfs4 125.212.251.104:6666 60B0409FF87E05CD1C88A7FB01E221E54A05EEA4 cert=K8j50CA2dN9mNF0sHB5nfI/AUMUxJumJejv7gl9IaIsJ2vS0icfexHJygze9/sXlimTAJQ iat-mode=0
-Bridge obfs4 95.216.186.68:1192 CD6A3B2F22509345DB47F64B2A26FB6A601A1FA0 cert=1gRAX5LV2NQGphZgGTwQumnh01/LwThkRbUmiX7wwAJARWxx2hjBAT3dhbpy/QoyqRmZHg iat-mode=0
+Bridge obfs4 209.6.217.38:21550 A85BB204DBAB68B754FECEFF883B2F97A6FA71CD cert=tKWQMfH+fJSp5QobD1yQOm2BNZTZgmf3btXYsYQPXT5wd+I0rCvuz4jEbNuUhVnkC2hKcA iat-mode=0
+Bridge obfs4 2.243.164.25:46595 4A34AF5AB467ECAE4291BF8F3BE5A151D93182F8 cert=7NQBqs0znoEz+7OUBwON+sSasp7CdrH9kXXhx0nMoLvXYlYBUnhYxvbXBgIzw6wu4ZI6Hw iat-mode=0
 '''
 
         self.banner_t = r"""     
